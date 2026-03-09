0c7711324b46a004b81c8ce7af71f3a3e00a5e4f
ravavarapuveerendra-hub <ravavarapuveerendra@gmail.com>
2026-03-09 17:02:37 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 st.image("CCRLogo.png", width=200)
-st.title("CCR GmbH : RF Plasma Source Comparison Dashboard")
+st.title("RF Plasma Source Comparison Dashboard")
 
 # -----------------------------
 # Load Excel file automatically
